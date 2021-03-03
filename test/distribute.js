86d05152938bd226de2e11d7d7a06e9ccbc52bbe
Carl Larson <cslarson@gmail.com>
2021-03-02 19:49:26 -0500

    log transaction data

--- test/distribute.js
+++ test/distribute.js
@@ -60,28 +60,24 @@ describe("Distribute", function() {
     .fill()
     .map(_ => l2_distribution.splice(0, MAX_BATCH_SIZE))
 
-    // console.log(batches)
-    batches.forEach((b,i)=>{
-      console.log(`\n!!! BATCH ${i} !!!\n`)
-      console.log(`["${b.map(a=>a.address).join('","')}"]`)
-      console.log(`["${b.map(a=>parseEther(a.donut.toString()).toString()).join('","')}"]`)
-    })
-
-
-    // expect(l2_distribution.length).to.equal(recipientsBatch1.length+recipientsBatch2.length)
-    // expect(l2_distribution.length).to.equal(amountsBatch1.length+amountsBatch2.length)
-    // console.log(`# recipients: ${l2_distribution.length}`)
-
-    // const recipients = [account1.address, account2.address]
-    // const amounts = [parseEther("100"), parseEther("200")]
-
     let approveTx = await token.connect(multisigSigner).approve(distribute.address, ethers.constants.MaxUint256)
-    // console.log(approveTx)
 
-    let distributeTx1 = await distribute.connect(multisigSigner).distribute(batches[0].map(a=>a.address), batches[0].map(a=>parseEther(a.donut.toString())), token.address)
-    console.log("gasUsed:", (await distributeTx1.wait()).gasUsed)
+    async function doBatch(b,i) {
+      console.log(`\n!!! BATCH ${i} inputs !!!\n`)
+      let addresses = b.map(a=>a.address)
+      let amounts = b.map(a=>parseEther(a.donut.toString()))
+      console.log(`["${addresses.join('","')}"]`)
+      console.log(`["${amounts.map(a=>a.toString()).join('","')}"]`)
+
+      let distributeTx = await distribute.connect(multisigSigner).distribute(addresses, amounts, token.address)
+      console.log(`\n!!! BATCH ${i} data !!!\n`)
+      console.log(distributeTx.data)
+      console.log(`\n!!! BATCH ${i} gas !!!\n`)
+      console.log("gasUsed:", (await distributeTx.wait()).gasUsed)
+    }
+
+    await Promise.all(batches.map(doBatch))
 
     expect(await token.balanceOf(batches[0][0].address)).to.equal(parseEther(batches[0][0].donut.toString()))
-    // expect(await token.balanceOf(l2_distribution[l2_distribution.length-1].address)).to.equal(parseEther(l2_distribution[l2_distribution.length-1].donut.toString()))
   });
 });
