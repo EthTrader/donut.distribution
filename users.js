0e933277c67cc6f93f4095c248969cefac5577ae
Carl Larson <cslarson@gmail.com>
2021-04-22 12:17:05 -0400

    move users.json to docs/ so it can be hosted easily from github

--- users.js
+++ users.js
@@ -11,7 +11,7 @@ const provider = new ethers.getDefaultProvider('mainnet', {
 })
 const MerkleTwoDropABI = require("./abi/MerkleTwoDropABI")
 const airdrop = new ethers.Contract(process.env.AIRDROP_ADDRESS, MerkleTwoDropABI, provider)
-const currentUsers = require("./out/users").reduce((p,{username,address})=>{p[username]={username,address};return p;},{})
+const currentUsers = require("./docs/users").reduce((p,{username,address})=>{p[username]={username,address};return p;},{})
 let hashes = {
   "0x1c034aed470bc1e7ed69f9e751b147d23a1470e95c3c28ff8a795617bf881840": "QmSK2S8cAHBRJW1BkKianGZazaTZdMqgHKLDmu123XzNKu", // aggDist
   "0xd16105af9df8954f03626c3ebb74b0f8e7baf8fd0ab27a311b8b18f8874d2f4f": "QmYB4LQFcMjuCkd7tLKUtjEnW29vM9qtiuW9RAExZWcmz2"  // round_96
@@ -45,7 +45,7 @@ async function main(){
   const newFileNameBase = `${__dirname}/out/users_${new Date().toISOString().slice(0,10)}`
   fs.writeFileSync(`${newFileNameBase}.json`, JSON.stringify(Object.values(users), null, 2))
   // fs.unlinkSync(`${__dirname}/out/users.json`)
-  fs.copyFileSync(`${newFileNameBase}.json`, `${__dirname}/out/users.json`)
+  fs.copyFileSync(`${newFileNameBase}.json`, `${__dirname}/docs/users.json`)
   // fs.symlinkSync(`${newFileNameBase}.json`, `${__dirname}/out/users.json`)
   const csvOut = await jsonexport(Object.values(users))
   fs.writeFileSync(`${newFileNameBase}.csv`, csvOut)
