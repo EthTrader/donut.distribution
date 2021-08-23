1d85af79aa0d8a35432ba859ce46d2075406a1d6
Carl Larson <cslarson@gmail.com>
2021-08-23 17:19:34 -0400

    generate new users.json

--- users.js
+++ users.js
@@ -13,7 +13,11 @@ const mainnet = new getDefaultProvider('mainnet', {
     projectSecret: env.INFURA_PROJECT_SECRET
   }
 })
-const xdai = new providers.JsonRpcProvider("https://xdai-archive.blockscout.com/")
+
+// const XDAI_JSON_RPC = "https://xdai.poanetwork.dev"
+const XDAI_JSON_RPC = "http://ethnode:8545"
+// const xdai = new providers.WebSocketProvider("wss://rpc.xdaichain.com/wss")
+const xdai = new providers.JsonRpcProvider(XDAI_JSON_RPC)
 
 const MerkleTwoDropABI = require("./abi/MerkleTwoDropABI")
 const ERC20 = require("./abi/ERC20")
@@ -33,7 +37,8 @@ let hashes = {
   "0xd16105af9df8954f03626c3ebb74b0f8e7baf8fd0ab27a311b8b18f8874d2f4f": "QmYB4LQFcMjuCkd7tLKUtjEnW29vM9qtiuW9RAExZWcmz2", // round_96
   "0x80ad7c3838aeec599c2882e0e4002d3d1840c3981cf63bc9a55c735e94fec79b": "QmWXyT6zRod9tHsoi9wvMaqVdJAvRhXKwuEXuTCjx44Rrd", // round_97
   "0x2504597d143a4e2c6453860311c5bf0dc1b930a92db0718443eb5905d1d0e091": "QmW7Tr1dVVfqUGMAFeEinHRkSCRumirNwJdLbbHj2YfwDv",  // round_98
-  "0x6cb9402ccf826bd6cedcb4d98a72dc527ee862ca8bec292f0f0e030af8b69972": "QmNwPTHUTCiw6B2nq6jZfd13vrxtGkKJYqq9to4nQZACU1"  // round_99
+  "0x6cb9402ccf826bd6cedcb4d98a72dc527ee862ca8bec292f0f0e030af8b69972": "QmNwPTHUTCiw6B2nq6jZfd13vrxtGkKJYqq9to4nQZACU1",  // round_99
+  "0x4b3d3769e1e640fc68c9ec97cc393562887f7d4f1ff8b1c2784af80a1ca7ba8a": "QmVESP61B4NwWvqQqtVdjyNLG62a6ryUK7LrwMJK45TTBq" // round_100
 }
 
 main()
