38866c79078a43293e85bb67be4d3c4c7f94f0d4
Carl Larson <cslarson@gmail.com>
2021-11-15 17:09:55 -0500

    update users.js

--- users.js
+++ users.js
@@ -1,11 +1,20 @@
-const { ethers } = require("ethers")
+import { ethers } from "ethers"
+
+// const { ethers } = require("ethers")
 const { constants, providers, Contract, getDefaultProvider } = ethers
 const { env } = process
 const { WeiPerEther } = constants
-const fetch = require('node-fetch')
-const jsonexport = require('jsonexport')
-const fs = require("fs")
-const Promise = require("bluebird")
+
+import fetch from "node-fetch"
+// const fetch = require('node-fetch')
+import jsonexport from "jsonexport"
+// const jsonexport = require('jsonexport')
+import fs from "fs"
+// const fs = require("fs")
+import Promise from "bluebird"
+// const Promise = require("bluebird")
+
+
 const mainnet = new getDefaultProvider('mainnet', {
   alchemy: env.ALCHEMY_API,
   infura: {
@@ -15,13 +24,18 @@ const mainnet = new getDefaultProvider('mainnet', {
 })
 
 // const XDAI_JSON_RPC = "https://xdai.poanetwork.dev"
-const XDAI_JSON_RPC = "http://ethnode:8545"
+// const XDAI_JSON_RPC = "http://ethnode:8545"
+const XDAI_JSON_RPC = "https://dai.poa.network/"
 // const xdai = new providers.WebSocketProvider("wss://rpc.xdaichain.com/wss")
 const xdai = new providers.JsonRpcProvider(XDAI_JSON_RPC)
 
-const MerkleTwoDropABI = require("./abi/MerkleTwoDropABI")
-const ERC20 = require("./abi/ERC20")
-const Staking = require("./abi/Staking")
+const MerkleTwoDropABI = JSON.parse(fs.readFileSync('./abi/MerkleTwoDropABI.json'));
+// const MerkleTwoDropABI = require("./abi/MerkleTwoDropABI")
+const ERC20 = JSON.parse(fs.readFileSync('./abi/ERC20.json'));
+// const ERC20 = require("./abi/ERC20")
+const Staking = JSON.parse(fs.readFileSync('./abi/Staking.json'));
+// const Staking = require("./abi/Staking")
+
 const UniToken = ERC20.concat(["function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast)"]);
 const airdropInstance = new Contract(env.AIRDROP_ADDRESS, MerkleTwoDropABI, mainnet)
 const contribInstance = new Contract(env.CONTRIB_ADDRESS, ERC20, mainnet)
@@ -31,7 +45,10 @@ const stakingMainnetInstance = new Contract(env.STAKING_MAINNET_ADDRESS, Staking
 const lpMainnetInstance = new Contract(env.LP_MAINNET_ADDRESS, UniToken, mainnet)
 const stakingXDaiInstance = new Contract(env.STAKING_XDAI_ADDRESS, Staking, xdai)
 const lpXDaiInstance = new Contract(env.LP_XDAI_ADDRESS, UniToken, xdai)
-const currentUsers = require("./docs/users").reduce((p,{username,address})=>{p[username]={username,address};return p;},{})
+
+const currentUsers = JSON.parse(fs.readFileSync('./docs/users.json')).reduce((p,{username,address})=>{p[username]={username,address};return p;},{})
+// const currentUsers = require("./docs/users")
+
 let hashes = {
   "0x1c034aed470bc1e7ed69f9e751b147d23a1470e95c3c28ff8a795617bf881840": "QmSK2S8cAHBRJW1BkKianGZazaTZdMqgHKLDmu123XzNKu", // aggDist
   "0xd16105af9df8954f03626c3ebb74b0f8e7baf8fd0ab27a311b8b18f8874d2f4f": "QmYB4LQFcMjuCkd7tLKUtjEnW29vM9qtiuW9RAExZWcmz2", // round_96
@@ -39,7 +56,9 @@ let hashes = {
   "0x2504597d143a4e2c6453860311c5bf0dc1b930a92db0718443eb5905d1d0e091": "QmW7Tr1dVVfqUGMAFeEinHRkSCRumirNwJdLbbHj2YfwDv",  // round_98
   "0x6cb9402ccf826bd6cedcb4d98a72dc527ee862ca8bec292f0f0e030af8b69972": "QmNwPTHUTCiw6B2nq6jZfd13vrxtGkKJYqq9to4nQZACU1",  // round_99
   "0x4b3d3769e1e640fc68c9ec97cc393562887f7d4f1ff8b1c2784af80a1ca7ba8a": "QmVESP61B4NwWvqQqtVdjyNLG62a6ryUK7LrwMJK45TTBq", // round_100
-  "0xc7ae82b5a66c994317b8cb2f07b89a975510e7941536478ac656f0843e9818b3": "QmawS3Q117PNppt2SQHcfFKii2wGpGFh8wPpA4AhwmL6qx" // round_101
+  "0xc7ae82b5a66c994317b8cb2f07b89a975510e7941536478ac656f0843e9818b3": "QmawS3Q117PNppt2SQHcfFKii2wGpGFh8wPpA4AhwmL6qx", // round_101
+  "0x4934cf7ec01671ead9c3355a8c9cc202f611b275d5f4d9ff27adb9bff560858f": "QmedigsXvwVXKQMkr3X2y1jxdUMKnPtXoAq4wBkgetnwJz",  // round_102
+  "0x2b9c711c4457dd4444f28f8828779a3b52f2d6cb7d0a1e6d5635b08df03a5d4f": "QmcGkH33Z2egCweCapEPiKzojQnYdL4pZGfAASqAx72mQA"  // round_103
 }
 
 main()
