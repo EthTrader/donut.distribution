1baf6cd9b9a9fc691a27ec43d2dfa04bbc433cfc
Hugo <hfenaux@gmail.com>
2021-12-07 14:22:04 -0500

    update users.json

--- users.js
+++ users.js
@@ -1,9 +1,14 @@
 import { ethers } from "ethers"
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // const { ethers } = require("ethers")
 const { constants, providers, Contract, getDefaultProvider } = ethers
 const { env } = process
 const { WeiPerEther } = constants
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 import fetch from "node-fetch"
 // const fetch = require('node-fetch')
@@ -24,8 +29,8 @@ const mainnet = new getDefaultProvider('mainnet', {
 })
 
 // const XDAI_JSON_RPC = "https://xdai.poanetwork.dev"
-// const XDAI_JSON_RPC = "http://ethnode:8545"
-const XDAI_JSON_RPC = "https://dai.poa.network/"
+const XDAI_JSON_RPC = "http://192.168.1.18:8545/"
+// const XDAI_JSON_RPC = "https://dai.poa.network/"
 // const xdai = new providers.WebSocketProvider("wss://rpc.xdaichain.com/wss")
 const xdai = new providers.JsonRpcProvider(XDAI_JSON_RPC)
 
@@ -53,12 +58,13 @@ let hashes = {
   "0x1c034aed470bc1e7ed69f9e751b147d23a1470e95c3c28ff8a795617bf881840": "QmSK2S8cAHBRJW1BkKianGZazaTZdMqgHKLDmu123XzNKu", // aggDist
   "0xd16105af9df8954f03626c3ebb74b0f8e7baf8fd0ab27a311b8b18f8874d2f4f": "QmYB4LQFcMjuCkd7tLKUtjEnW29vM9qtiuW9RAExZWcmz2", // round_96
   "0x80ad7c3838aeec599c2882e0e4002d3d1840c3981cf63bc9a55c735e94fec79b": "QmWXyT6zRod9tHsoi9wvMaqVdJAvRhXKwuEXuTCjx44Rrd", // round_97
-  "0x2504597d143a4e2c6453860311c5bf0dc1b930a92db0718443eb5905d1d0e091": "QmW7Tr1dVVfqUGMAFeEinHRkSCRumirNwJdLbbHj2YfwDv",  // round_98
-  "0x6cb9402ccf826bd6cedcb4d98a72dc527ee862ca8bec292f0f0e030af8b69972": "QmNwPTHUTCiw6B2nq6jZfd13vrxtGkKJYqq9to4nQZACU1",  // round_99
+  "0x2504597d143a4e2c6453860311c5bf0dc1b930a92db0718443eb5905d1d0e091": "QmW7Tr1dVVfqUGMAFeEinHRkSCRumirNwJdLbbHj2YfwDv", // round_98
+  "0x6cb9402ccf826bd6cedcb4d98a72dc527ee862ca8bec292f0f0e030af8b69972": "QmNwPTHUTCiw6B2nq6jZfd13vrxtGkKJYqq9to4nQZACU1", // round_99
   "0x4b3d3769e1e640fc68c9ec97cc393562887f7d4f1ff8b1c2784af80a1ca7ba8a": "QmVESP61B4NwWvqQqtVdjyNLG62a6ryUK7LrwMJK45TTBq", // round_100
   "0xc7ae82b5a66c994317b8cb2f07b89a975510e7941536478ac656f0843e9818b3": "QmawS3Q117PNppt2SQHcfFKii2wGpGFh8wPpA4AhwmL6qx", // round_101
-  "0x4934cf7ec01671ead9c3355a8c9cc202f611b275d5f4d9ff27adb9bff560858f": "QmedigsXvwVXKQMkr3X2y1jxdUMKnPtXoAq4wBkgetnwJz",  // round_102
-  "0x2b9c711c4457dd4444f28f8828779a3b52f2d6cb7d0a1e6d5635b08df03a5d4f": "QmcGkH33Z2egCweCapEPiKzojQnYdL4pZGfAASqAx72mQA"  // round_103
+  "0x4934cf7ec01671ead9c3355a8c9cc202f611b275d5f4d9ff27adb9bff560858f": "QmedigsXvwVXKQMkr3X2y1jxdUMKnPtXoAq4wBkgetnwJz", // round_102
+  "0x2b9c711c4457dd4444f28f8828779a3b52f2d6cb7d0a1e6d5635b08df03a5d4f": "QmcGkH33Z2egCweCapEPiKzojQnYdL4pZGfAASqAx72mQA", // round_103
+  "0x11f394d28e9be1fbcecfd5634e9ec3c9d5cc4d34b080dce287d18955fc16e56d": "QmQKKzZMhE33qepK7NSmHV6pSp5PpT5uY8sfRte1GHxTnx"  // round_104
 }
 
 main()
