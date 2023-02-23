07848997b9354198c2d1d60be04c32510cf0ed0c
kohrts <hfenaux@gmail.com>
2023-02-23 12:44:54 +0000

    removed users round 120

--- ineligible.js
+++ ineligible.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const inactive = []
 
-const LABEL = `round_119`
+const LABEL = `round_120`
 const FILE = `${LABEL}.csv`
 const DATE = Math.floor(Date.now() / 1000) - 5184000 
 
