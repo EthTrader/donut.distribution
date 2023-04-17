5a575f2260ad23016697d7622e62486c1d8c637a
kohrts <28346089+huugx@users.noreply.github.com>
2023-04-17 11:34:46 +0000

    round 122 removed

--- ineligible.js
+++ ineligible.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const inactive = []
 
-const LABEL = `round_121`
+const LABEL = `round_122`
 const FILE = `${LABEL}.csv`
 const DATE = Math.floor(Date.now() / 1000) - 5184000 
 
