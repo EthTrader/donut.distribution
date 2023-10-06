9bea776430a897033dc8ebed7f835e81654c15f8
kohrts <28346089+huugx@users.noreply.github.com>
2023-10-06 13:35:19 +0000

    Revert "round 128 ineligible users v2"
    This reverts commit 07d080aef3d569170ead94c4199ebbd8171ef252.

--- ineligible.js
+++ ineligible.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const inactive = []
 
-const LABEL = `round_128`
+const LABEL = `round_127`
 const FILE = `${LABEL}.csv`
 const DATE = Math.floor(Date.now() / 1000) - 5184000 
 
